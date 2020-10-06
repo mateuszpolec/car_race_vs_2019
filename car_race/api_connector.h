660a019b4d0b8d475ce1fbf568cb4f3c3459ddd6
HoopDead <polec0@wp.pl>
2020-10-06 13:40:27 +0200

    Made it possible to parse JSON

--- car_race/api_connector.h
+++ car_race/api_connector.h
@@ -10,6 +10,7 @@ public:
     void getAuthToken() {
         // Create a new HTTP client
         sf::Http http;
+        JSONCPP_STRING err;
         Json::Value root;
         // Host for localhost, REMBEMER to turn on Django API
         http.setHost("127.0.0.1", 8000);
@@ -22,8 +23,17 @@ public:
         if (status == sf::Http::Response::Ok)
         {
             const std::string rawJson = response.getBody();
+            const auto rawJsonLength = static_cast<int>(rawJson.length());
             std::cout << rawJson << std::endl;
             std::cout << "[api_connector.h] Passing JSON to const string" << std::endl;
+            Json::CharReaderBuilder builder;
+            const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+            if (!reader->parse(rawJson.c_str(), rawJson.c_str() + rawJsonLength, &root,
+                &err)) {
+                std::cout << "error" << std::endl;
+            }
+            const std::string name = root["token"].asString();
+            std::cout << name;
         }
         else
         {
