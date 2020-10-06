6d3d7ed5bfa555f21e0aa4e1a2a9932ef0c066fb
HoopDead <polec0@wp.pl>
2020-10-06 18:53:05 +0200

    Documentation - player_class.h

--- car_race/Player/player_class.h
+++ car_race/Player/player_class.h
@@ -1,11 +1,24 @@
 #include <iostream>
 
+
+/**
+* Class Player - handle information about player
+* 
+*/
 class Player {
     int x;
     int y;
+    std::string token;
 
 public:
-    Player(int x_position, int y_position) {
+    /**
+    * Constructor
+    * @param x_position - X-axis player position
+    * @param y_position - Y-axis player position
+    * @param authtoken - Authentication token for API communication
+    */
+    Player(int x_position, int y_position, std::string authtoken) {
+        token = authtoken;
         x = x_position;
         y = y_position;
     };
