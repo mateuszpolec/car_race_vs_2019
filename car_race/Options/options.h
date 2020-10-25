7180084d1d8f72ee7611da3c052146e636d6c056
HoopDead <polec0@wp.pl>
2020-10-25 18:08:06 +0100

    Changed max velocity and acceleration of player

--- car_race/Options/options.h
+++ car_race/Options/options.h
@@ -5,11 +5,11 @@ class Options {
 	public:
 
 		// Static variables and options
-		float playerMaxVelocityXAxis = 1200.f;
-		float playerMaxVelocityYAxis = 1200.f;
+		float playerMaxVelocityXAxis = 1400.f;
+		float playerMaxVelocityYAxis = 1400.f;
 
-		float playerMaxAccelerationXAxis = 13.f;
-		float playerMaxAccelerationYAxis = 13.f;
+		float playerMaxAccelerationXAxis = 16.f;
+		float playerMaxAccelerationYAxis = 16.f;
 
 		// Methods
 
